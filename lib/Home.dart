1cc9566081fb83fcceacb59ca04cafc7b685b612
Thejani Saranaguptha <thejanis@cbctechsol.com>
2020-10-16 18:12:27 +0530

    second commit for flutter geolocation

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/basic.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,10 +10,53 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
+  String locationMessage = "";
+
+  void getCurrentLocation() async {
+    Position position =
+        await getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position);
+
+    setState(() {
+      locationMessage = "Latitude:" +
+          "${position.latitude}" +
+          "\n" +
+          "Longitude:" +
+          "${position.longitude}";
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFE0E0E0E0),
       body: Center(
-        child: Text('Welcome to Home'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: Text(
+                "Location",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, left: 20.0),
+              child: Center(
+                child: Text(locationMessage),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
